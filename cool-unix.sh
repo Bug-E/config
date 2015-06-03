aa3e5fcdd87a4914208af76c5fd23a72b9919b3f
Dheeraj Agarwal <dheeru.agarwal@gmail.com>
2015-06-03 18:58:35 +0530

    mysql output to csv

--- cool-unix.sh
+++ cool-unix.sh
@@ -1,1 +1,5 @@
 grep --color='auto' -P -n "[\x80-\xFF]" file_name # finds out all non ascii characters
+awk '{print NF}'
+
+##  convert the output from mysql to csv
+awk -F\| -v OFS=',' 'gsub(/ /, "", $2);gsub(/ /, "", $3);gsub(/ /, "", $4); { print $2,$3,$4}'
