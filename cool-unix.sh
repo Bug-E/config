796e167108a643f96b893fbea021c9a01db38d23
Dheeraj Agarwal <dheeru.agarwal@gmail.com>
2016-11-09 17:41:57 +0530

    Update cool-unix.sh

--- cool-unix.sh
+++ cool-unix.sh
@@ -3,3 +3,6 @@ awk '{print NF}'
 
 ##  convert the output from mysql to csv
 awk -F\| -v OFS=',' 'gsub(/ /, "", $2);gsub(/ /, "", $3);gsub(/ /, "", $4); { print $2,$3,$4}'
+
+## Update the currently editing file in vim with sudo credentials
+:w ! sudo tee %
