19efc1a8b6c68cbec5fc793a45de18d5b71d4166
Dheeraj Agarwal <dheeru.agarwal@gmail.com>
2016-11-09 23:46:02 +0530

    added dart setup

--- setup.sh
+++ setup.sh
@@ -29,3 +29,17 @@ cp config/.vimrc ~/.vimrc
 cp config/.gitconfig ~/.gitconfig
 cp config/.gitignore ~/.gitignore
 ####
+
+### dart setup
+read -p "Do you want to install Dart language? y/n" response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+	sudo apt-get install apt-transport-https
+	# Get the Google Linux package signing key.
+	sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+	# Set up the location of the stable repository.
+	sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+	sudo apt-get update
+	sudo apt-get install dart
+fi
+####
