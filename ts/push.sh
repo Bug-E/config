5bcf9589c0b946dba4be5a1854ff2106b75621dd
Dheeraj Agarwal <dheeru.agarwal@gmail.com>
2020-05-28 14:22:42 +0530

    Create push.sh

---
+++ ts/push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ip="172.18.77.1"
+if [ ! -z $1 ]; then ip=$1; fi
+echo "+gradle :thoughtspot-common:generateSources"
+gradle :thoughtspot-common:generateSources
+echo '+gradle :callosum-server:build  -x test'
+gradle :callosum-server:build  -x test
+cmd="rsync callosum/server/target/callosum-server-1.0-SNAPSHOT.war admin@${ip}:/usr/local/scaligent/release/callosum/server/target/callosum-server-1.0-SNAPSHOT.war"
+echo "+$cmd"
+eval cmd
